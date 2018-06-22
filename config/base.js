e41d26fa1c4444786a3d925d8e128eee2145c4ca
Alexander <1eksandr999@gmail.com>
2018-06-22 23:44:32 +0300

    Hw_8

--- config/base.js
+++ config/base.js
@@ -12,6 +12,7 @@ const baseConf = (_path) => {
         index: ['babel-polyfill', './src/index/index.js'],
         lesson_6: ['babel-polyfill', './src/lesson_6/lesson_6.js'],
         hw_1: ['babel-polyfill', './src/hw_1/hw_1.js'],
+        lesson_7: ['babel-polyfill', './src/lesson_7/lesson_7.js'],
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
@@ -72,6 +73,13 @@ const baseConf = (_path) => {
                     ]
                 },
                 {
+                    test: /\.css/,
+                    loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'autoprefixer-loader?browsers=last 5 version',]
+                    })
+                },
+                {
                     test: /\.scss/,
                     loader: ExtractTextPlugin.extract({
                         fallback: 'style-loader',
@@ -89,11 +97,11 @@ const baseConf = (_path) => {
                      * You can add here any file extension you want to get copied to your output
                      */
                     test: /\.(png|jpg|jpeg|gif|svg)$/,
-                    loader: 'file-loader?publicPath=../&name=assets/images/[name].[ext]'
+                    loader: 'file-loader?name=assets/images/[name].[ext]'
                 },
                 {
                     test: /\.(eot|ttf|woff|woff2)$/,
-                    loader: 'file-loader?publicPath=../&name=assets/fonts/[name].[ext]'
+                    loader: 'file-loader?name=assets/fonts/[name].[ext]'
                 }
             ]
         },
---
+++ src/lesson_7/lesson_7.js
@@ -0,0 +1,43 @@
+import './lesson_7.scss';
+
+// let userName = 'Sasha';
+// let userAge = 18;
+// let userAddress = 'ass asass assas';
+// let userPhoneNumber = '+23423423423';
+//
+// let user = {
+//     name: 'Sasha',
+//     age: 26,
+//     address: 'asas asasd asdds',
+//     phoneNumber: '+123123'
+// };
+//
+
+// let product = {
+//     Iphone: '200',
+//     title: 'Iphone 7',
+//     imageUrl: 'http;//ws3.com/image12.jpg'
+// };
+//
+// let product2 = {
+//     price: '200',
+//     macBook: '2000',
+//     s9: '1000'
+// };
+//
+// const name = prompt('Enter your name ','');
+//
+// for (let i = 0; i < name.length; i++) {
+//     console.log(name[i]);
+// }
+//
+// for( let key in product2) {
+//     console.log('KEY: ' + key);
+//     console.log ('VALUE :' + product2[key]);
+// }
+
+// console.log('DONE');
+// const keyForSearch = 'price';
+// const total = parseInt(product[keyForSearch] + parseInt(product2.price);
+
+
